f138b780b54aa30bd30e85c82a1143d365a3d926
Athanasios Ioannidis <108409836+ThanasisIoannidis@users.noreply.github.com>
2022-10-03 17:38:55 +0100

    Add files via upload
    Login

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome malaka");
+Console.WriteLine("Enter username:");
+string userName = Console.ReadLine();
+if (userName == "userName")
+{
+    Console.WriteLine(userName + " You are loged in!");
+}
+while (userName != "userName")
+{
+    Console.WriteLine("The username is wrong. please try again");
+    userName = Console.ReadLine();
+}
